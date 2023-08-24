6c1f01ea90185a774b66cb56f53635a09ea93088
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-08-24 15:16:27 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,33 +18,31 @@ function setup() {
   
    var ball_options = {
     /*restitution: Isso controla a elasticidade de um objeto, ou seja, o quão bem ele irá quicar quando colidir com outras superfícies. */
-    restitution: 0.95,
+    //????
     /*Isso simula a resistência do ar ao movimento do objeto. Quanto maior o valor, mais resistência do ar o objeto terá. */
-    frictionAir:0.01
+    //????
   }
 
   var rock_options = {
-    restitution: 0.85,
+    //????
 
   }
    
    var ground_options ={
-     isStatic: true
+     //????
    };
   
 
   ground = Bodies.rectangle(200,390,400,20,ground_options);
   World.add(world,ground);
 
-  wall = Bodies.rectangle(300,200,200,20,ground_options);
-  World.add(world,wall);
+  //criar wall(chão)
 
   ball = Bodies.circle(100,10,20,ball_options);
   World.add(world,ball);
   
-  ball2 = Bodies.circle(250,10,20,rock_options);
-  World.add(world,ball2);
-
+  //criar ball2(bola2)
+  
   rectMode(CENTER);
   ellipseMode(RADIUS);
 }
@@ -55,12 +53,14 @@ function draw()
   background(51);
   Engine.update(engine);
   
-  /*Esta linha desenha uma elipse (círculo) na posição atual da bola (ball.position.x e ball.position.y). A última entrada 20 define o raio da elipse, */
+  /*Esta linha desenha uma elipse (círculo) na posição atual da bola (ball.position.x e ball.position.y).
+  A última entrada 20 define o raio da elipse, */
+  
   ellipse(ball.position.x,ball.position.y,20);
   rect(ground.position.x,ground.position.y,400,20);
 
-  ellipse(ball2.position.x,ball2.position.y,20);
-  rect(wall.position.x,wall.position.y,200,20);
+  //crie a bola 2 e wall (parede)
+  //???
  
 }
 
